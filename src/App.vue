8bf40ed221b03eb2a3d0969e48ebb2d1102f2c7d
dingzhen <dingzhen@qq.com>
2025-02-25 13:09:35 +0800

    feat: 引入less，element-plus，axios，hooks，createDialog

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <el-button>124</el-button>
+</template>
 
 <script lang="ts" setup></script>
 
---
+++ src/common/index.ts
@@ -0,0 +1,3 @@
+import * as UTILS from "@/common/utils";
+
+export { UTILS };
